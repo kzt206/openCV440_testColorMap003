467ef53912274b7267a958d53198290ca5b56c86
KAZUO TSUTSUI <kzt206@gmail.com>
2020-09-06 10:35:22 +0900

    modify countour1

--- openCV440_testColorMap003/testContour1.cpp
+++ openCV440_testColorMap003/testContour1.cpp
@@ -5,6 +5,60 @@ using namespace cv;
 
 void axis(Point x0, Point x1, Point y0, Point y1, Mat& image);
 
+
+int ColorScaleBCGYR(double in_value)
+{
+	// 0.0～1.0 の範囲の値をサーモグラフィみたいな色にする
+	// 0.0                    1.0
+	// 青    水    緑    黄    赤
+	// 最小値以下 = 青
+	// 最大値以上 = 赤
+	int ret;
+	int a = 255;    // alpha値
+	int r, g, b;    // RGB値
+	double  value = in_value;
+	double  tmp_val = cos(4 * 3.1415 * value);
+	int     col_val = (int)((-tmp_val / 2 + 0.5) * 255);
+	if (value >= (4.0 / 4.0)) { r = 255;     g = 0;       b = 0; }   // 赤
+	else if (value >= (3.0 / 4.0)) { r = 255;     g = col_val; b = 0; }   // 黄～赤
+	else if (value >= (2.0 / 4.0)) { r = col_val; g = 255;     b = 0; }   // 緑～黄
+	else if (value >= (1.0 / 4.0)) { r = 0;       g = 255;     b = col_val; }   // 水～緑
+	else if (value >= (0.0 / 4.0)) { r = 0;       g = col_val; b = 255; }   // 青～水
+	else { r = 0;       g = 0;       b = 255; }   // 青
+	ret = (a & 0x000000FF) << 24
+		| (r & 0x000000FF) << 16
+		| (g & 0x000000FF) << 8
+		| (b & 0x000000FF);
+	return ret;
+}
+
+
+void ColorScaleBCGYR(int &r,int &g,int &b,int z)
+{
+	// 0.0～1.0 の範囲の値をサーモグラフィみたいな色にする
+	// 0.0                    1.0
+	// 青    水    緑    黄    赤
+	// 最小値以下 = 青
+	// 最大値以上 = 赤
+	//int ret;
+	int a = 255;    // alpha値
+	//int r, g, b;    // RGB値
+	double  value = z/255.;
+	double  tmp_val = cos(4 * 3.1415 * value);
+	int     col_val = (int)((-tmp_val / 2 + 0.5) * 255);
+	if (value >= (4.0 / 4.0)) { r = 255;     g = 0;       b = 0; }   // 赤
+	else if (value >= (3.0 / 4.0)) { r = 255;     g = col_val; b = 0; }   // 黄～赤
+	else if (value >= (2.0 / 4.0)) { r = col_val; g = 255;     b = 0; }   // 緑～黄
+	else if (value >= (1.0 / 4.0)) { r = 0;       g = 255;     b = col_val; }   // 水～緑
+	else if (value >= (0.0 / 4.0)) { r = 0;       g = col_val; b = 255; }   // 青～水
+	else { r = 0;       g = 0;       b = 255; }   // 青
+	//ret = (a & 0x000000FF) << 24
+	//	| (r & 0x000000FF) << 16
+	//	| (g & 0x000000FF) << 8
+	//	| (b & 0x000000FF);
+	//return ret;
+}
+
 void testContour1() {
 
 	// image object
@@ -19,8 +73,8 @@ void testContour1() {
 	const int yMax = yShift + yLength + yMaxShift;
 
 	// point size
-	const int dx = 10;
-	const int dy = 10;
+	const int dx = 5;
+	const int dy = 5;
 
 
 	Mat image(yMax, xMax, CV_8UC3);
@@ -43,7 +97,7 @@ void testContour1() {
 		for (int j = 0; j < numberData; j++) {
 			int tmpx = i * dx;
 			int tmpy = j * dy;
-			double tmpz = pow(tmpx-300,2) + pow(tmpy-100,2);
+			double tmpz =  pow(tmpx-250,2) + pow(tmpy,2);
 			if (maxZ < tmpz) maxZ = tmpz;
 			if (minZ > tmpz) minZ = tmpz;
 			x[n] = tmpx;
@@ -64,11 +118,26 @@ void testContour1() {
 		}
 	}
 
-
+	int halfZ = 256/2;
 	// draw rectangles 4
 	for (int i = 0; i < sizeof(x) / sizeof(*x); i++) {
 		Rect rect(0 + x[i] + xShift, yMax - dy - y[i] - yShift, dx, dy);  // (x, y, width, height)
-		rectangle(image, rect, Scalar(z[i], z[i], z[i]), -1, LINE_4); // Scalar(Blue,Green,Red)  -1:fill
+		int r, g, b;
+		/*if (z[i] < halfZ) {
+			r = 0;
+			g = int(255. / (halfZ - 0) * (z[i] - 0));
+			b = int(255. - 255 / (halfZ - 0) * (z[i] - 0));
+		}
+		else {
+			r = int(255. / (255-halfZ) * (z[i] - halfZ));
+			g = int(255. - 255 / (255- halfZ) * (z[i] - halfZ));
+			b = 0;
+		}*/
+
+		ColorScaleBCGYR(r, g, b, z[i]);
+
+		Scalar color(b,g,r);
+		rectangle(image, rect, color, -1, LINE_4); // Scalar(Blue,Green,Red)  -1:fill
 	}
 
 	//axis
@@ -80,7 +149,7 @@ void testContour1() {
 
 	//Show image
 	imshow("Map", image);
-	//imwrite("contour1.jpg", image);
+	imwrite("contour1.jpg", image);
 	waitKey();
 
 
