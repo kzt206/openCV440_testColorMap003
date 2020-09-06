66b106b4f9d0dfd5e018fd17482197f04b5fda80
KAZUO TSUTSUI <kzt206@gmail.com>
2020-09-06 12:14:53 +0900

    devide to new file of readData

--- openCV440_testColorMap003/main.cpp
+++ openCV440_testColorMap003/main.cpp
@@ -7,7 +7,7 @@ using namespace cv;
 void testRect3();
 void testContour1();
 void testContour2();
-int testReadData(double *x, double *y, double *z);
+int testReadData(string infile,double *x, double *y, double *z);
 
 int main()
 {
@@ -16,7 +16,8 @@ int main()
 	//testContour2();
 
 	double x[10000], y[10000], z[10000];
-	testReadData(x,y,z);
+	string infile = "C:\\Users\\kzt20\\source\\repos\\openCV440_testColorMap003\\openCV440_testColorMap003\\testdata.txt";
+	testReadData(infile,x,y,z);
 
     for (int i = 0; i < 4;i++) {
         cout << x[i] << ',' << y[i] << ',' << z[i] << endl;
@@ -24,40 +25,5 @@ int main()
 
 
 	return 0;
-
-}
-
-int testReadData(double *x, double *y, double *z) {
-    std::ifstream ifs("C:\\Users\\kzt20\\source\\repos\\openCV440_testColorMap003\\openCV440_testColorMap003\\testdata.txt");
-    std::string str,s;
-
-    if (ifs.fail()) {
-        std::cerr << "Failed to open file." << std::endl;
-        return -1;
-    }
-
-    int i = 0;
-    
-    while (getline(ifs, str)) {
-        std::cout  << str << std::endl;
-        stringstream ss{ str };
-        vector<string> v;
-        while(getline(ss, s, ' ')) {
-            v.push_back(s);
-        }
-        
-        /*for (const string& s : v) {
-            cout << s << endl;
-        }*/
-
-        x[i] = stod(v[0]);
-        y[i] = stod(v[1]);
-        z[i] = stod(v[2]);
-
-
-        i++;
-    }
-
-    ifs.close();
 
 }
---
+++ openCV440_testColorMap003/testReadData.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int testReadData(string infile, double* x, double* y, double* z) {
+    std::ifstream ifs(infile);
+    std::string str, s;
+
+    if (ifs.fail()) {
+        cerr << "Failed to open file." << std::endl;
+        return -1;
+    }
+
+    int i = 0;
+
+    while (getline(ifs, str)) {
+        std::cout << str << std::endl;
+        stringstream ss{ str };
+        vector<string> v;
+        while (getline(ss, s, ' ')) {
+            v.push_back(s);
+        }
+
+        /*for (const string& s : v) {
+            cout << s << endl;
+        }*/
+
+        x[i] = stod(v[0]);
+        y[i] = stod(v[1]);
+        z[i] = stod(v[2]);
+
+
+        i++;
+    }
+
+    ifs.close();
+
+}
